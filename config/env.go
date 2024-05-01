dbc2e89eb2f7e857d9be93eae803699a305e7a99
Rafael Lima <santisraeusou@gmail.com>
2024-05-01 18:25:25 -0300

    refactor: ajuste envs

--- config/env.go
+++ config/env.go
@@ -9,7 +9,6 @@ import (
 )
 
 type environment struct {
-	Port             int    `env:"PORT" required:"true"`
 	PostgresHost     string `env:"POSTGRES_HOST" required:"true"`
 	PostgresPort     int    `env:"POSTGRES_PORT" default:"5432"`
 	PostgresUser     string `env:"POSTGRES_USER" required:"true"`
