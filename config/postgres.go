5ad37d8a558db595ff85c59c62fe0d2949384c8e
Rafael Lima <santisraeusou@gmail.com>
2024-05-02 18:03:12 -0300

    ajuste dsn

--- config/postgres.go
+++ config/postgres.go
@@ -9,7 +9,7 @@ import (
 )
 
 func InitializePostgres() (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require", Environment.PostgresHost, Environment.PostgresPort, Environment.PostgresUser, Environment.PostgresPassword, Environment.PostgresDb)
+	dsn := fmt.Sprintf("host=%s user=%s port=%d password=%s dbname=%s sslmode=require", Environment.PostgresHost, Environment.PostgresUser, Environment.PostgresPort, Environment.PostgresPassword, Environment.PostgresDb)
 	fmt.Println(dsn)
 	db, err := gorm.Open(postgres.Open(dsn))
 	if err != nil {
