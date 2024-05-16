40a7fae2935115659c1ec8f4d8e7cad142610b30
Rafael Lima <santisraeusou@gmail.com>
2024-05-16 19:51:40 -0300

    cors

--- router/router.go
+++ router/router.go
@@ -1,9 +1,13 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/guicazaroto/learning-go/util"
+)
 
 func Initialize() {
 	r := gin.Default()
+	r.Use(util.CORSMiddleware())
 
 	initializeRoutes(r)
 	r.Run()
